c2d9e08b33a770716bb85bfa52431323bbd18219
Diar Ghaderi <diar.ghaderi@gmail.com>
2024-08-18 00:11:48 +0300

    initial commit

---
+++ secrets.h
@@ -0,0 +1,6 @@
+// Use this file to store all the private credentials
+
+#define SECRET_SSID "SSID HERE"
+#define SECRET_PASS "PASSWORD HERE"
+#define CLIENT_ID "CLIENT ID"
+#define CLIENT_SECRET "CLIENT SECRET"
