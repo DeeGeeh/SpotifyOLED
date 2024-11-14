14d4b4e43c8b0e9b774c18853a59066f859a5abb
D <147754073+DeeGeeh@users.noreply.github.com>
2024-11-15 01:48:17 +0200

    Create secrets.h

---
+++ spotscreen/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all the private credentials.
+
+#define SECRET_SSID "WIFI SSID"
+#define SECRET_PASS "WIFI PASSWORD"
+
+// Still figuring these out
+#define CLIENT_ID "CLIENT ID"
+#define CLIENT_SECRET "CLIENT SECRET"
