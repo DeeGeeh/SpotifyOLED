6368554262904f4ca3551067fe61e242be038cd4
Diar Ghaderi <diar.ghaderi@gmail.com>
2024-11-15 00:45:39 +0200

    add template secrets.h

--- secrets.h
+++
@@ -1,6 +0,0 @@
-// Use this file to store all the private credentials
-
-#define SECRET_SSID "SSID HERE"
-#define SECRET_PASS "PASSWORD HERE"
-#define CLIENT_ID "CLIENT ID"
-#define CLIENT_SECRET "CLIENT SECRET"
