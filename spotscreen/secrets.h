cbd853a13cafc07dae2fdb2fe8adbaeaf1278ca4
D <147754073+DeeGeeh@users.noreply.github.com>
2024-12-23 19:30:14 +0200

    Create secrets.h

---
+++ spotscreen/secrets.h
@@ -0,0 +1,5 @@
+// Use this file to store all the private credentials.
+
+#define SECRET_SSID "" // WiFi ssid
+#define SECRET_PASS "" // Wifi password
+#define SECRET_CLIENT_ID "" // Get this from your spotify developer dashboard
